cbcf58772afb1b295c7b15058c8d1739cf43ef09
jittarinkan <jittarin.kan@gmail.com>
2022-05-21 18:59:30 +0700

    feat: add pagination to index

--- app/controllers/family_members_controller.rb
+++ app/controllers/family_members_controller.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 class FamilyMembersController < ApplicationController
   before_action :set_family_member, only: %i[ show ]
 
   def index
     @family_members = FamilyMember.all
+                                  .page(params[:page])
+                                  .per(params[:per])
   end
 
   def new
@@ -13,7 +17,7 @@ class FamilyMembersController < ApplicationController
     @family_member = FamilyMember.new(create_family_member_params)
 
     if @family_member.save
-      redirect_to @family_member, notice: "Family member was successfully created."
+      redirect_to family_members_path, success: "Successfully created family member."
     else
       render :new, status: :unprocessable_entity
     end
